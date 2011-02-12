7be23489182add78c15967f35bba4b59ff5d3bf2
Hiroki Kuzuno <kuzuno@gmail.com>
2011-02-12 15:13:57 +0900

    parse my page html to show favorite community's live

--- python/niconama.py
+++ python/niconama.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import re
 import sys
 import urllib
 import urllib2
@@ -9,12 +10,17 @@ import socket
 import struct
 import cookielib
 import argparse
+from HTMLParser import HTMLParser
 
 class NicoLive:
 
   LOGIN_URL = 'https://secure.nicovideo.jp/secure/login'
+  ANTENA_URL = LOGIN_URL + '?site=nicolive_antenna'
+  ALERTSTATUS_URL = 'http://live.nicovideo.jp/api/getalertstatus'
+
   GETPLAYSTATUS_URL = 'http://watch.live.nicovideo.jp/api/getplayerstatus?v=%s'
   THREAD_TEMPLATE = '<thread thread="%s" version="20061206" res_from="-1"/>'
+  MY_URL = 'http://live.nicovideo.jp/my'
 
   def __init__(self):
     self.login_status = False
@@ -22,27 +28,20 @@ class NicoLive:
     self.addr = None
     self.port = None
 
-
   def login(self, params):
     cookie = cookielib.CookieJar()
     opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))
     urllib2.install_opener(opener)
-    postdata = urllib.urlencode(params)
-    request = urllib2.Request(self.LOGIN_URL, postdata)
-    res = urllib2.urlopen(request)
+    res = self._urlopen(self.LOGIN_URL, params)
     if cookie._cookies['.nicovideo.jp']:
       self.login_status = True
 
   def watch(self, lv, cb):
-    if self.login_status is not True:
+    if self.login_status is False:
       print 'you haven\'t login'
       exit(0)
-
     lv_url = self.GETPLAYSTATUS_URL % lv
-    request = urllib2.Request(lv_url)
-    res = urllib2.urlopen(request)
-    data = res.read()
-    dom = etree.fromstring(data)
+    dom = self._urlopen(lv_url, None, etree.fromstring)
     status = dom.findall('.')[0].attrib
     if status['status'] == 'fail':
       print 'program was finished or login failed'
@@ -75,17 +74,90 @@ class NicoLive:
         print 'unhandled exception'
         exit(0)
 
+  def _urlopen(self, url, params=None, f=lambda x: x):
+    request = urllib2.Request(url)
+    if params != None:
+      postdata = urllib.urlencode(params)
+      res = urllib2.urlopen(request, postdata)
+    else:
+      res = urllib2.urlopen(request)
+    data = res.read()
+    return f(data)
+
+  def antena(self, params):
+    dom = self._urlopen(self.ANTENA_URL, params, etree.fromstring)
+    ticket = ''
+    for item in dom.findall('.//'):
+      if item.tag == 'ticket':
+        ticket = item.text
+    dom = self._urlopen(self.ALERTSTATUS_URL, {'ticket':ticket}, etree.fromstring)
+    community_id_list = []
+    addr = None
+    port = None
+    thread = None
+    for item in dom.findall('.//'):
+      if item.tag == 'community_id':
+        community_id_list.append(item.text)
+      elif item.tag == 'addr':
+        addr = item.text
+      elif item.tag == 'port':
+        port = item.text
+      elif item.tag == 'thread':
+        thread = item.text
+    print addr, port
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((addr, int(port)))
+    msg =  self.THREAD_TEMPLATE % (thread)
+    while True:
+      sock.send(msg)
+      sock.send(struct.pack('b',0))
+      data = sock.recv(1024)
+
+  def my(self):
+    if self.login_status is False:
+      print 'you haven\'t login'
+      exit(0)
+    data = self._urlopen(self.MY_URL)
+    myparser = MyHTMLParser()
+    myparser.feed(data)
+    favorite_lv_set = set(myparser.lv_list)
+    if len(favorite_lv_set) == 0:
+      print 'no favorite community\'s live'
+    else:
+      for lv in favorite_lv_set:
+        print '%s : %s' % (lv[0], lv[1])
+
+class MyHTMLParser(HTMLParser):
+
+  WATCH_RE = re.compile('http://live.nicovideo.jp/watch/(lv[0-9]+)', re.I)
+
+  def __init__(self):
+    HTMLParser.__init__(self)
+    self.lv_list = []
+
+  def handle_starttag(self, tag, attrs):
+    if tag == 'a' and attrs:
+      if len(attrs) > 1 and attrs[0][0] == 'href':
+        watch_re = self.WATCH_RE.search(attrs[0][1])
+        if watch_re:
+          self.lv_list.append((watch_re.group(1), attrs[1][1]))
+
 def main():
   def _print_msg(msg):
     print msg
   parser = argparse.ArgumentParser(description='NicoLive Comment View')
-  parser.add_argument('-lv', type=str,
-                      help='target nicolive lv')
+  parser.add_argument('-lv', type=str, help='target nicolive lv')
+  parser.add_argument('-my', action='store_true', help='show favorite community\'s live')
   args = parser.parse_args()
-  if args.lv:
-    params = {'mail':'YOUR@MAILADDRESS', 'password':'YOURPASSWORD'}
-    nicolive = NicoLive()
-    nicolive.login(params)
+
+  params = {'mail':'YOUR@MAILADDRESS', 'password':'YOURPASSWORD'}
+  nicolive = NicoLive()
+  nicolive.login(params)
+
+  if args.my:
+    #nicolive.antena(params)
+    nicolive.my()
+  elif args.lv:
     nicolive.watch(args.lv, _print_msg)
   else:
     print parser.print_help()
